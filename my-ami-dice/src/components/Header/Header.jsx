744455ca060e2e8051f08525e9892c4a52fdd9a0
root <root@DESKTOP-SIH6VNN.localdomain>
2023-01-21 12:12:26 +0100

    upload

--- my-ami-dice/src/components/Header/Header.jsx
+++ my-ami-dice/src/components/Header/Header.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-/* import { Button } from 'semantic-ui-react'; */
-import LoginSigninModal from '../LoginSigninModal/LoginSigninModal';
-
+import React, { useState } from 'react';
+import { Button } from 'semantic-ui-react';
+import InviteModal from '../InviteModal/InviteModal'
 import './style.scss';
 
 function Header() {
+
+  const [isConnect, setIsConnect] = useState(true);
+
   return (
     <header className='header'>
         <div className='header-container'>
-          <div>
-            <h1 className='header-container-title'>
-              MyAmiDice
-            </h1>
-          </div>
 
-          <LoginSigninModal />
+          { isConnect? <InviteModal/> : null }
+          <h1 className='header-container-title'>
+              MyAmiDice
+          </h1>
+          <Button className='header-container-buttonconnect' negative>Connexion</Button>
         </div>
+        
     </header>
   )
 }
