2aaf064827616ff532c94bb485513d3591259868
root <root@DESKTOP-SIH6VNN.localdomain>
2023-01-21 18:04:31 +0100

    creation profile page and modify profile in progress

--- my-ami-dice/src/components/Header/Header.jsx
+++ my-ami-dice/src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { Button } from 'semantic-ui-react';
 import InviteModal from '../InviteModal/InviteModal'
 import './style.scss';
@@ -21,12 +21,12 @@ function Header() {
     <header className='header'>
         <div className='header-container'>
 
-          { onProfil? <Button>Accueil</Button> : null }
+          { onProfil? <Button as={NavLink} to="/" >Accueil</Button> : null }
           { isConnect? <InviteModal/> : null }
           <h1 className='header-container-title'>
               MyAmiDice
           </h1>
-          { isConnect? <Button>Mon Profil</Button> : null}
+          { isConnect? <Button as={NavLink} to="/home/profile">Mon Profil</Button> : null}
           <Button className='header-container-buttonconnect' negative>Connexion</Button>
         </div>
         
--- my-ami-dice/src/components/Profile/Profile.jsx
+++ my-ami-dice/src/components/Profile/Profile.jsx
@@ -1,12 +1,44 @@
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import { Button } from 'semantic-ui-react';
 import './style.scss'
+
+const user = {
+    pseudo: "Provencal",
+    firstName: "Perceval",
+    lastName: "DeGalle",
+    email: "gercevalDeGalle@chevalier.com"
+  }
+
+
+
 
 function Profile() {
 
   return (
         <>
         <Header />
+        <div className='profile-container'>
+          <div className='profile'>
+            <h1>Mon Profil</h1>
+            <div className='profile--pseudo'>
+              <p>Pseudo: {user.pseudo}</p>              
+            </div>
+            <div className='profile--firstName'>
+              <p>Prénom: {user.firstName}</p>              
+            </div>
+            <div className='profile--pseudo'>
+              <p>Nom: {user.lastName}</p>
+            </div>
+            <div className='profile--pseudo'>
+              <p>Email: {user.email} </p>
+            </div>
+            <div className='profile-buttons'>
+              <Button>Modifier</Button>  {//faire la gestion des modales pour modifier le profil un boutton modifier par elememt et un modale par button}
+              <Button>Supprimer</Button>              
+            </div>
+          </div>
+        </div>
         <Footer />
         </>
   )
