2b48107c0bcd6d816f48bae80ada89103ace2e13
Guillaume Darroux <g.darroux@gmail.com>
2023-01-23 11:07:22 +0100

    Demo page static

--- my-ami-dice/src/components/App/App.jsx
+++ my-ami-dice/src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import Demo from '../Demo/Demo';
 import Home from '../Home/Home';
 import HomeMember from '../HomeMember/HomeMember';
 import Profile from '../Profile/Profile'
@@ -11,8 +12,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/home/user/" element={<HomeMember />} />
       <Route path="/home/profile/" element={<Profile />} />
+      <Route path="/demo/" element={<Demo />} />
       {/* <Route path="/home/admin/" element={< />} />
-      <Route path="/demo/" element={< />} />
       <Route path="/home/creategame/" element={< />} />
       <Route path="/home/gameroom/:id" element={< />} />
       <Route path="*" element={< />} /> */}
--- my-ami-dice/src/components/Demo/Demo.jsx
+++ my-ami-dice/src/components/Demo/Demo.jsx
@@ -1,17 +1,63 @@
 import React from 'react';
 
+import './style.scss';
+import logo from '../../assets/images/logoDice.jpg'
 import Header from '../Header/Header';
-
 import Footer from '../Footer/Footer';
 
 function Demo() {
+
+/* const features = [
+    {
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos deserunt reiciendis, voluptate vero molestiae itaque vel dolorum, voluptatibus necessitatibus eum eos eaque earum tempore quidem eius laboriosam assumenda ut in.",
+      image: logo
+    },
+    {
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos deserunt reiciendis, voluptate vero molestiae itaque vel dolorum, voluptatibus necessitatibus eum eos eaque earum tempore quidem eius laboriosam assumenda ut in.",
+      image: logo
+    },
+    {
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos deserunt reiciendis, voluptate vero molestiae itaque vel dolorum, voluptatibus necessitatibus eum eos eaque earum tempore quidem eius laboriosam assumenda ut in.",
+      image: logo
+    }
+  ]
+ */
+
   return (
     <div className='home'>
-        <Header />
-        <div>
+      <Header />
+        <div className='feature'>
           
+              <div className='feature-container'>
+                <div className='feature-container-text'>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos deserunt reiciendis, voluptate vero molestiae itaque vel dolorum, voluptatibus necessitatibus eum eos eaque earum tempore quidem eius laboriosam assumenda ut in.
+                </div>
+                <img className='feature-container-image' src={logo} alt="logo-MyAmiDice" />
+              </div>
+          
+            <div className='feature-container'>
+              <img className='feature-container-image' src={logo} alt="logo-MyAmiDice" />
+              <div className='feature-container-text'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos deserunt reiciendis, voluptate vero molestiae itaque vel dolorum, voluptatibus necessitatibus eum eos eaque earum tempore quidem eius laboriosam assumenda ut in.
+              </div>
+            </div>
+            
+            <div className='feature-container'>
+              <div className='feature-container-text'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos deserunt reiciendis, voluptate vero molestiae itaque vel dolorum, voluptatibus necessitatibus eum eos eaque earum tempore quidem eius laboriosam assumenda ut in.
+              </div>
+              <img className='feature-container-image' src={logo} alt="logo-MyAmiDice" />
+            </div>
+
+            <div className='feature-container'>
+              <img className='feature-container-image' src={logo} alt="logo-MyAmiDice" />
+              <div className='feature-container-text'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos deserunt reiciendis, voluptate vero molestiae itaque vel dolorum, voluptatibus necessitatibus eum eos eaque earum tempore quidem eius laboriosam assumenda ut in.
+              </div>
+            </div>
+
         </div>
-        <Footer />
+      <Footer />
     </div>
   )
 }
