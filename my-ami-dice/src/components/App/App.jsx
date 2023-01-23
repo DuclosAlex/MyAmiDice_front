0f41c6314cfe09b6a44452e0bfb83e5a8e83ac25
Guillaume Darroux <g.darroux@gmail.com>
2023-01-23 09:53:40 +0100

    minor changes to app.jsx

--- my-ami-dice/src/components/App/App.jsx
+++ my-ami-dice/src/components/App/App.jsx
@@ -9,8 +9,13 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="home/user/" element={<HomeMember />} />
-      <Route path="home/profile/" element={<Profile />} />
+      <Route path="/home/user/" element={<HomeMember />} />
+      <Route path="/home/profile/" element={<Profile />} />
+      {/* <Route path="/home/admin/" element={< />} />
+      <Route path="/demo/" element={< />} />
+      <Route path="/home/creategame/" element={< />} />
+      <Route path="/home/gameroom/:id" element={< />} />
+      <Route path="*" element={< />} /> */}
     </Routes>
   )
 }
---
+++ my-ami-dice/src/components/Demo/Demo.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Header from '../Header/Header';
+
+import Footer from '../Footer/Footer';
+
+function Demo() {
+  return (
+    <div className='home'>
+        <Header />
+        <div>
+          
+        </div>
+        <Footer />
+    </div>
+  )
+}
+
+export default Demo
