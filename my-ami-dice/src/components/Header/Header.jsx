4cc4bfdb11295cab9cf068ef490b4f64f9397310
Guillaume Darroux <g.darroux@gmail.com>
2023-01-24 14:40:34 +0100

    character creation and header modifications

--- my-ami-dice/src/components/Header/Header.jsx
+++ my-ami-dice/src/components/Header/Header.jsx
@@ -11,20 +11,20 @@ function Header() {
   const [isConnect, setIsConnect] = useState(true);
   
   const location = useLocation();
+  
+  // On vérifie l'url pour savoir sur quelle page l'utilisteur est
   const onProfil = location.pathname.includes("profile");
   const onDemo = location.pathname.includes("demo");
-
-  
-  
-
+  const onCreateGame = location.pathname.includes("creategame");
 
 
   return (
     <header className='header'>
         <div className='header-container'>
 
-          { onProfil? <Button as={NavLink} to="/" >Accueil</Button> : null }; {/* Affiche le bouton Accueil lorsque l'utilisateur est sur la page Profil */}
-          { onDemo? <Button as={NavLink} to="/" >Accueil</Button> : null }; {/* Affiche le bouton Accueil lorsque l'utilisateur est sur la page Demo */}
+          { onProfil? <Button as={NavLink} to="/" >Accueil</Button> : null } {/* Affiche le bouton Accueil lorsque l'utilisateur est sur la page Profil */}
+          { onDemo? <Button as={NavLink} to="/" >Accueil</Button> : null } {/* Affiche le bouton Accueil lorsque l'utilisateur est sur la page Demo */}
+          { onCreateGame? <Button as={NavLink} to="/" >Accueil</Button> : null } {/* Affiche le bouton Accueil lorsque l'utilisateur est sur la page Création de partie */}
           { isConnect? <InviteModal/> : null }
           <h1 className='header-container-title'>
               MyAmiDice
