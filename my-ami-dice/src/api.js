b52aa906db8eef812133e5852a053a2bb2a16cc2
Guillaume Darroux <g.darroux@gmail.com>
2023-01-25 11:38:20 +0100

    characterCreation items & skills ok, only 1 of each. axios ok, check when back is deployed

--- my-ami-dice/src/api.js
+++ my-ami-dice/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:5173'
+    baseURL: 'https://projet-littleroll20-back.vercel.app/'
 });
 
 export default api;
