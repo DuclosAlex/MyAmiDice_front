fd1c632440509b401a0bb6ff1702bd001cfe9aa6
Guillaume Darroux <g.darroux@gmail.com>
2023-01-25 17:35:46 +0100

    ChatRoom and installation of socket.io

---
+++ my-ami-dice/src/components/ChatRoom/ChatRoom.jsx
@@ -0,0 +1,115 @@
+//import { io } from "socket.io-client";
+
+import React, { useState } from 'react';
+import { Button, Form } from "semantic-ui-react";
+import "./style.scss";
+
+function ChatRoom() {
+/*
+DEBUT SOCKET.IO
+*/
+    // Connexion à socket.io côté serveur
+    //const socket = io("http://localhost:3000"); //TODO: régler les cors
+    
+    // On écoute l'évènement "connect"
+  /*   socket.on("connect", () => {
+        displayMessage(`Connexion avec l'id : ${socket.id}`);
+    })
+
+    // On écoute l'évènement "receive-message"
+    socket.on("receive-message", message => {
+        displayMessage(message);
+    }) */
+
+/*
+FIN SOCKET.IO
+*/
+    
+    const [message, setMessage] = useState("");
+    const [room, setRoom] = useState("");
+    const [chatMessages, setChatMessages] = useState([]);
+    
+    // Mise à jour du state quand on écrit dans l'input correspondant
+    function handleChangeMessage(event) {
+        setMessage(event.target.value)
+    }
+    
+    function handleChangeRoom(event) {
+        setRoom(event.target.value)
+    }
+    // Déjà géré par le state par le handleChangeRoom
+    /*    function handleClick(event) {
+    console.log("room : ",room);
+} */
+
+function handleSubmit(event) {
+    event.preventDefault();
+        
+    // Si "message" est vide, on ne fait rien
+    if(message === "") return;
+    
+    displayMessage(message);
+    
+    // On envoie l'évènement "send-message" au serveur avec "message" dans la room "room"
+    //socket.emit("send-message", message, room) //TODO:
+    
+    setMessage("");
+    
+    console.log("message : ", message);
+    
+}
+
+function displayMessage(message) {
+    console.log("dans le displayMessage message : ", message);
+        setChatMessages(prevState => ([
+            ...prevState,
+            message
+        ]))
+    }
+
+  return (
+    <>
+        <div className="chatroom">
+            <ul className="message-container" >
+                {chatMessages.map((chatMessage) => (
+                <li>{chatMessage}</li>
+            ))}
+            </ul>
+            <Form id="form" onSubmit={handleSubmit} >
+                <Form.Group>
+                    <Form.Input
+                        label="Message"
+                        type="text"
+                        name="message-input"
+                        value={message}
+                        onChange={handleChangeMessage}
+                        inline
+                    />
+                    <Button
+                        type="submit"
+                        id="send-button"
+                    >Envoyer
+                    </Button>
+                    <Form.Input
+                        label="Salon"
+                        type="text"
+                        name="room-input"
+                        value={room}
+                        onChange={handleChangeRoom}
+                        inline
+                    >
+                    </Form.Input>
+                    <Button
+                        type="button"
+                        id="room-button"
+                        //onClick={handleClick}
+                    >Rejoindre
+                    </Button>
+                </Form.Group>
+            </Form>
+        </div>
+    </>
+  )
+}
+
+export default ChatRoom
--- my-ami-dice/src/components/Home/Home.jsx
+++ my-ami-dice/src/components/Home/Home.jsx
@@ -5,12 +5,14 @@ import Header from '../Header/Header';
 import Slideshow from '../Slideshow/Slideshow';
 import Footer from '../Footer/Footer';
 import NewMemberList from '../NewMemberList/NewMemberList';
+import ChatRoom from '../ChatRoom/ChatRoom';
 
 function Home() {
   return (
     <div className='home'>
         <Header />
         <Slideshow />
+        <ChatRoom />
         <Concept />
         <NewMemberList />
         <Footer />
