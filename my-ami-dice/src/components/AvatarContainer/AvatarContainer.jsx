35b1306346bf89d0d92de3ee665a93274f65fa3d
root <root@DESKTOP-SIH6VNN.localdomain>
2023-01-26 12:44:00 +0100

    add v0.1 feature of gameroom + add few style

---
+++ my-ami-dice/src/components/AvatarContainer/AvatarContainer.jsx
@@ -0,0 +1,48 @@
+import './style.scss';
+import player1IMG from '../../assets/images/joueur1.jpg'
+import player2IMG from '../../assets/images/joueur2.jpg'
+import player3IMG from '../../assets/images/joueur3.jpg'
+import player4IMG from '../../assets/images/joueur4.jpg'
+import { useState } from 'react';
+
+
+
+function AvatarContainer() {
+
+    const [namePlayer, setNamePlayer] = useState('')
+    
+    const handleClick = (event) =>{
+        console.log ("fdngskdfhg", event)
+        const namePlayer = event.target.name
+        setNamePlayer(namePlayer)
+        
+    }
+
+  return (
+    <div className='avatars-container'>
+        <div className='container'>
+            <div className='avatar-player'>
+                <img onClick={handleClick} src={player1IMG} name="joueur1" alt="Avatar" />
+            </div>
+            <div className='avatar-player'>
+                <img onClick={handleClick} src={player2IMG} name="joueur2" alt="Avatar" />
+            </div>
+            <div className='avatar-player'>
+                <img onClick={handleClick} src={player3IMG} name="joueur3" alt="Avatar" />
+            </div>
+            <div className='avatar-player'>
+                <img onClick={handleClick} src={player4IMG} name="joueur4" alt="Avatar" />
+            </div> 
+        </div>     
+        
+        {namePlayer? <div className='character-sheet'>
+                <p>Fiche perso du {namePlayer} </p>
+        </div>: null}
+        
+
+
+    </div>
+  )
+}
+
+export default AvatarContainer
--- my-ami-dice/src/components/GameRoom/GameRoom.jsx
+++ my-ami-dice/src/components/GameRoom/GameRoom.jsx
@@ -1,7 +1,9 @@
 import DiceGenerator from "../DiceGenerator/DiceGenerator";
 import ContextGameRoom from "../ContextGameRoom/ContextGameRoom";
 import ChatRoom from "../ChatRoom/ChatRoom";
+import Map from "../Map/Map"
 import "./style.scss";
+import AvatarContainer from "../AvatarContainer/AvatarContainer";
 
 function GameRoom() {
 
@@ -10,8 +12,13 @@ function GameRoom() {
   return (
     <ContextGameRoom.Provider value = {5}>
       <div className='GameRoom'>
-         <DiceGenerator />
-         <ChatRoom />
+        <div className="avatar">
+            <AvatarContainer />
+        </div>
+
+        <Map />
+        <DiceGenerator />
+        <ChatRoom />
       </div>
     </ContextGameRoom.Provider>
   )
