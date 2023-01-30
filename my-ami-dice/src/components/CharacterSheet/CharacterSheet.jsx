1b6180605cea8f6d9535ab0f6b85a7318de9b617
root <root@DESKTOP-SIH6VNN.localdomain>
2023-01-30 11:24:39 +0100

    first archi characterSheet

--- my-ami-dice/src/components/CharacterSheet/CharacterSheet.jsx
+++ my-ami-dice/src/components/CharacterSheet/CharacterSheet.jsx
@@ -4,8 +4,69 @@ import './style.scss';
 function CharacterSheet() {   
 
   return (
-    <div className='character-sheet'>
-      ficheperso
+    <div className='characterSheet'>
+      <div className="headContainer">
+        <img className="character-avatar" src="" alt="character-avatar" />
+        <div className="info-character">
+          <div className="presentaton-character">
+            <div>nom</div>
+            <div>prenom</div>
+            <div>race</div>
+            <div>classe</div>
+            <div>lvl</div>
+          </div>
+          <div className="lifepoint-mana">
+            <div className="lifePoint">
+              <div>PV:</div>
+              <div>barreStat</div>
+            </div>
+            <div className="manaPoint">
+              <div>PM:</div>
+              <div>barreStat</div>
+            </div>
+          </div>
+        </div>
+        <div className="description-character">
+          <p>Description du charatere</p>
+        </div>
+        <div>
+          <div className="characteristic-character">
+            <div>
+              <div>FOR:</div>
+              <div>QTY</div>
+            </div>
+            <div>
+              <div>DEX:</div>
+              <div>QTY</div>
+            </div>
+            <div>
+              <div>WIS:</div>
+              <div>QTY</div>
+            </div>
+            <div>
+              <div>CHA:</div>
+              <div>QTY</div>
+            </div>
+            <div>
+              <div>INT:</div>
+              <div>QTY</div>
+            </div>
+            <div>
+              <div>CONST:</div>
+              <div>QTY</div>
+            </div>            
+          </div>
+          <div>
+            <div>nom du skill</div>
+            <div>description du skill</div>
+          </div>
+        </div>
+        <div className="item-character">
+          <div>nom de l'item</div>
+          <div>quatité d'item</div>
+          <div>description de l'item</div>
+        </div>
+      </div>
     </div>
   )
 }
